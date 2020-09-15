1cbd9e61f2a563794c29466cde736d6f7a7c805d
PlaceChen <cwslive@live.com>
2020-09-15 20:58:33 +0800

    fixed some specific

--- src/main/java/chtmod/TrafficLight/StartupCommon.java
+++ src/main/java/chtmod/TrafficLight/StartupCommon.java
@@ -8,15 +8,18 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class StartupCommon {
 	public static TrafficLight[] tl = { new TrafficLight("B", 0), new TrafficLight("R", 0), new TrafficLight("T", 0),
 			new TrafficLight("Straight", 1), new TrafficLight("Straight1", 2), new TrafficLight("Straight2", 7),
-			new TrafficLight("Straight3", 8), new TrafficLight("Straight4", 3), new TrafficLight("Left", 1),
+			new TrafficLight("Straight3", 8), new TrafficLight("Straight4", 3), new TrafficLight("Straight5", 8),
+			new TrafficLight("Straight6", 7), new TrafficLight("Straight7", 7), new TrafficLight("Left", 1),
 			new TrafficLight("Left1", 2), new TrafficLight("Left2", 7), new TrafficLight("Left3", 8),
-			new TrafficLight("Left4", 3), new TrafficLight("Right", 1), new TrafficLight("Bike", 1),
+			new TrafficLight("Left4", 3), new TrafficLight("Left5", 8), new TrafficLight("Left6", 7),
+			new TrafficLight("Left7", 7), new TrafficLight("Right", 1), new TrafficLight("Bike", 1),
 			new TrafficLight("Bike1", 9), new TrafficLight("Bike2", 8), new TrafficLight("Bike3", 4),
 			new TrafficLight("Bike4", 10), new TrafficLight("Bike5", 0), new TrafficLight("Back", 1),
 			new TrafficLight("Circle", 1), new TrafficLight("Circle1", 3), new TrafficLight("Circle2", 7),
-			new TrafficLight("Circle3", 8), new TrafficLight("People", 1), new TrafficLight("People1", 5),
-			new TrafficLight("People2", 6), new TrafficLight("People3", 11), new TrafficLight("People4", 0),
-			new TrafficLight("People5", 5), new TrafficLight("L", 0) };
+			new TrafficLight("Circle3", 8), new TrafficLight("Circle4", 8), new TrafficLight("Circle5", 7),
+			new TrafficLight("Circle6", 8), new TrafficLight("Circle7", 7), new TrafficLight("People", 1),
+			new TrafficLight("People1", 5), new TrafficLight("People2", 6), new TrafficLight("People3", 11),
+			new TrafficLight("People4", 0), new TrafficLight("People5", 5), new TrafficLight("L", 0) };
 	public static ItemBlock[] itl;
 	public static BlockSignalProvider sp = new BlockSignalProvider();
 	public static ItemConnectTraffic ct = new ItemConnectTraffic();
