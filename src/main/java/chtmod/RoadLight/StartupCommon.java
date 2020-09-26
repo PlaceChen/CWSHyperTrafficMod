26e0aa1f74b4c04c97d37c6bc88e9d0fd1525c71
PlaceChen <cwslive@live.com>
2020-09-26 12:59:43 +0800

    fixed some specific

--- src/main/java/chtmod/RoadLight/StartupCommon.java
+++ src/main/java/chtmod/RoadLight/StartupCommon.java
@@ -6,8 +6,8 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class StartupCommon {
 	public static Light lights[] = new Light[] { new Light("RoadLight", 1), new Light("RoadLight0", 0, false),
 			new RotationLight("RoadLight1", 0), new RotationLight("RoadLight2", 1), new RotationLight("RoadLight3", 0),
-			new RotationLight("RoadLight4", 2), new RotationLight("RoadLight5", 0, false),
-			new RotationLight("RoadLight6", 0, false), new RotationLight("RoadLight7", 3),
+			new RotationLight("RoadLight4", 2), new RotationLight("RoadLight5", -1, false),
+			new RotationLight("RoadLight6", -1, false), new RotationLight("RoadLight7", 3),
 			new RotationLight("RoadLight8", 4, false), new Light("RoadLight9", 2, false),
 			new Light("RoadLight10", 3, false), new Light("RoadLight11", 0, false), new RotationLight("RoadLight12", 5),
 			new RotationLight("RoadLight13", 6) };
